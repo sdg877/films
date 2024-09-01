472bfd13ee1903f366a2fcc40a6543c48249fdbb
Sdg877 <sdrakegill@gmail.com>
2024-09-01 07:56:04 +0100

    activity update on backend implemented

--- fitness_tracker/backend/routes/activtyRoutes.js
+++ fitness_tracker/backend/routes/activtyRoutes.js
@@ -34,22 +34,53 @@ router.get("/", async (request, response) => {
     });
   } catch (error) {
     console.log(error.message);
-    response.status(500).send({ message: error.message })
+    response.status(500).send({ message: error.message });
   }
-})
+});
 
 router.get("/:id", async (request, response) => {
   try {
     const { id } = request.params;
     const activity = await Activity.findById(id);
-    
+
     if (!activity) {
-      return response.status(404).json({ message: "Activity not found"})
+      return response.status(404).json({ message: "Activity not found" });
     }
     return response.status(200).json(activity);
   } catch (error) {
     console.log(error.message);
-    response.status(500).send({ message: error.message })
+    response.status(500).send({ message: error.message });
+  }
+});
+
+router.put("/:id", async (request, response) => {
+  try {
+    const { date, time, activity, duration, difficulty } = request.body;
+
+    if (!date || !time || !activity || !duration || !difficulty) {
+      return response.status(400).send({
+        message:
+          "Please provide all required fields: date, time, activity, duration, difficulty",
+      });
+    }
+
+    const { id } = request.params;
+    const result = await Activity.findByIdAndUpdate(
+      id,
+      { date, time, activity, duration, difficulty },
+      { new: true }
+    );
+
+    if (!result) {
+      return response.status(404).json({ message: "Activity not found!" });
+    }
+
+    return response
+      .status(200)
+      .send({ message: "Acitvity updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
   }
 });
 
