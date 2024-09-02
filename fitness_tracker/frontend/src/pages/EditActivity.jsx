6f9b25eda0d95ad6a0b1fece6a945d4d7b226630
Sdg877 <sdrakegill@gmail.com>
2024-09-02 08:24:18 +0100

    frontend crud routes and pages added

---
+++ fitness_tracker/frontend/src/pages/EditActivity.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditActivity = () => {
+  return (
+    <div>
+      Edit Activity
+    </div>
+  )
+}
+
+export default EditActivity
---
+++ fitness_tracker/frontend/src/pages/ShowActivity.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShowActivity = () => {
+  return (
+    <div>
+      Show Activity
+    </div>
+  )
+}
+
+export default ShowActivity
