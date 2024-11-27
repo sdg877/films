08983297a526bce42aed28773a1daa072ca90019
Sdg877 <sdrakegill@gmail.com>
2024-11-27 10:24:42 +0000

    New auth page added

--- fitness_tracker/frontend/src/App.jsx
+++ fitness_tracker/frontend/src/App.jsx
@@ -6,44 +6,46 @@ import CreateActivity from "./components/CreateActivity";
 import ShowActivity from "./components/ShowActivity";
 import EditActivity from "./components/EditActivity";
 import DeleteActivity from "./components/DeleteActivity";
-import SignUpForm from "./components/SignUpForm";
-import LoginForm from "./components/LoginForm";
+import AuthPage from "./pages/AuthPage"; 
 
 const App = () => {
   const [user, setUser] = useState(null);
 
+
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
     if (storedUser) {
+      console.log("Stored user found:", storedUser);
       setUser(JSON.parse(storedUser));
+    } else {
+      console.log("No stored user found.");
     }
   }, []);
-
+  
   useEffect(() => {
     if (user) {
+      console.log("User state updated:", user);
       localStorage.setItem("user", JSON.stringify(user));
     } else {
+      console.log("User state cleared.");
       localStorage.removeItem("user");
     }
   }, [user]);
+  
 
   return (
     <BrowserRouter>
       <div>
         <Routes>
           <Route 
-            path="/" 
+            path="/activity" 
             element={<Home user={user} setUser={setUser} />} 
           />
           <Route path="/activity/create" element={<CreateActivity />} />
           <Route path="/activity/details/:id" element={<ShowActivity />} />
           <Route path="/activity/edit/:id" element={<EditActivity />} />
           <Route path="/activity/delete/:id" element={<DeleteActivity />} />
-          <Route
-            path="/users/create"
-            element={<SignUpForm setUser={setUser} />}
-          />
-          <Route path="/login" element={<LoginForm setUser={setUser} />} />
+          <Route path="/" element={<AuthPage setUser={setUser} />} />
         </Routes>
       </div>
     </BrowserRouter>
