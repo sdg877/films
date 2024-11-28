0eb6cd2a3f7466ee9e791ce59aed27d9ca45c2b2
Sdg877 <sdrakegill@gmail.com>
2024-11-28 08:27:35 +0000

    Is admin field added

--- fitness_tracker/backend/models/userModel.js
+++ fitness_tracker/backend/models/userModel.js
@@ -4,6 +4,7 @@ import bcrypt from "bcrypt";
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
 });
 
 userSchema.pre("save", async function (next) {
