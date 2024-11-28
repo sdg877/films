1a6a7a7732a46ebf8a8e9b3b95ef099c53e0ea44
Sdg877 <sdrakegill@gmail.com>
2024-11-28 10:29:05 +0000

    Removing console log

--- fitness_tracker/backend/middleware/authMiddleware.js
+++ fitness_tracker/backend/middleware/authMiddleware.js
@@ -1,20 +1,19 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const authenticateUser = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ error: 'Unauthorized' });
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Unauthorized" });
   }
 
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
 
   jwt.verify(token, process.env.SECRET, (err, decoded) => {
     if (err) {
-      return res.status(401).json({ error: 'Unauthorized' });
+      return res.status(401).json({ error: "Unauthorized" });
     }
 
-    console.log("Decoded JWT:", decoded);
-    req.user = decoded.user; 
+    req.user = decoded.user;
     next();
   });
 };
