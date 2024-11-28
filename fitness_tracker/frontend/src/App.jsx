e40b23e8b8a91edf7fcc0ed2b127b836341bccf1
Sdg877 <sdrakegill@gmail.com>
2024-11-28 08:29:11 +0000

    Auth context added to routes

--- fitness_tracker/frontend/src/App.jsx
+++ fitness_tracker/frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from './components/AuthContext'; 
 import Home from "./pages/Home";
 import CreateActivity from "./components/CreateActivity";
 import ShowActivity from "./components/ShowActivity";
 import EditActivity from "./components/EditActivity";
 import DeleteActivity from "./components/DeleteActivity";
-import AuthPage from "./pages/AuthPage"; 
+import AuthPage from "./pages/AuthPage";
+import SignUpForm from "./components/SignUpForm"; 
 
 const App = () => {
   const [user, setUser] = useState(null);
@@ -14,33 +16,34 @@ const App = () => {
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
     if (storedUser) {
-      setUser(JSON.parse(storedUser));
+      setUser(JSON.parse(storedUser)); 
     }
   }, []);
-  
+
   useEffect(() => {
     if (user) {
-      localStorage.setItem("user", JSON.stringify(user));
+      localStorage.setItem("user", JSON.stringify(user)); 
     } else {
-      localStorage.removeItem("user");
+      localStorage.removeItem("user"); 
     }
   }, [user]);
 
   return (
     <BrowserRouter>
-      <div>
+      <AuthProvider> 
         <Routes>
           <Route 
             path="/activity" 
-            element={<Home user={user} setUser={setUser} />} 
+            element={user ? <Home user={user} setUser={setUser} /> : <Navigate to="/" />} 
           />
-          <Route path="/activity/create" element={<CreateActivity />} />
-          <Route path="/activity/details/:id" element={<ShowActivity />} />
-          <Route path="/activity/edit/:id" element={<EditActivity />} />
-          <Route path="/activity/delete/:id" element={<DeleteActivity />} />
+          <Route path="/activity/create" element={user ? <CreateActivity /> : <Navigate to="/" />} />
+          <Route path="/activity/:id" element={user ? <ShowActivity /> : <Navigate to="/" />} />
+          <Route path="/activity/edit/:id" element={user ? <EditActivity /> : <Navigate to="/" />} />
+          <Route path="/activity/delete/:id" element={user ? <DeleteActivity /> : <Navigate to="/" />} />
           <Route path="/" element={<AuthPage setUser={setUser} />} />
+          <Route path="/signup" element={<SignUpForm setUser={setUser} />} />
         </Routes>
-      </div>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
