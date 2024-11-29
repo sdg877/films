351344e696e0e98b642eff53043bc1f2796af48e
Sdg877 <sdrakegill@gmail.com>
2024-11-29 09:49:04 +0000

    Successfully rendering all users activities

--- fitness_tracker/frontend/src/components/UserActivityTable.jsx
+++ fitness_tracker/frontend/src/components/UserActivityTable.jsx
@@ -1,28 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
 const UserActivityTable = ({ activities }) => {
   return (
     <table className="w-full border-collapse">
       <thead>
         <tr>
-          <th className="border border-slate-600 rounded-md">Username</th>
-          <th className="border border-slate-600 rounded-md">Date</th>
-          <th className="border border-slate-600 rounded-md">Time</th>
+          
           <th className="border border-slate-600 rounded-md">Activity</th>
+          <th className="border border-slate-600 rounded-md">Date</th>
           <th className="border border-slate-600 rounded-md">Duration (min)</th>
           <th className="border border-slate-600 rounded-md">Difficulty</th>
+          <th className="border border-slate-600 rounded-md max-md:hidden">User Name</th>
         </tr>
       </thead>
       <tbody>
-        {activities.map((activity) => (
+        {activities.map((activity, index) => (
           <tr key={activity._id} className="h-8">
-            <td className="border border-slate-700 text-center">{activity.username}</td>
-            <td className="border border-slate-700 text-center">{activity.date}</td>
-            <td className="border border-slate-700 text-center">{activity.time}</td>
+            
             <td className="border border-slate-700 text-center">{activity.activity}</td>
+            <td className="border border-slate-700 text-center">{activity.date}</td>
             <td className="border border-slate-700 text-center">{activity.duration}</td>
             <td className="border border-slate-700 text-center">{activity.difficulty}</td>
+            <td className="border border-slate-700 text-center">{activity?.user?.name}</td> 
           </tr>
         ))}
       </tbody>
