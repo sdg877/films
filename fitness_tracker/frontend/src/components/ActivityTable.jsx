de4c60bae8df979aee4b516f394964339b1e8063
Sdg877 <sdrakegill@gmail.com>
2024-11-29 08:30:55 +0000

    Activity row and table combined, update to styling

--- fitness_tracker/frontend/src/components/ActivityTable.jsx
+++ fitness_tracker/frontend/src/components/ActivityTable.jsx
@@ -1,9 +1,11 @@
-import React from "react";
-import ActivityRow from "./ActivityRow";
+import { Link } from "react-router-dom";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineDelete } from "react-icons/md";
 
 const ActivityTable = ({ activities }) => {
   return (
-    <table className="w-full border-separate border-spacing-2">
+    <table className="w-full border-collapse">
       <thead>
         <tr>
           <th className="border border-slate-600 rounded-md">No</th>
@@ -17,7 +19,27 @@ const ActivityTable = ({ activities }) => {
       </thead>
       <tbody>
         {activities.map((activity, index) => (
-          <ActivityRow key={activity._id} index={index} activity={activity} />
+          <tr key={activity._id} className="h-8">
+            <td className="border border-slate-700 text-center">{index + 1}</td>
+            <td className="border border-slate-700 text-center">{activity.date}</td>
+            <td className="border border-slate-700 text-center">{activity.time}</td>
+            <td className="border border-slate-700 text-center">{activity.activity}</td>
+            <td className="border border-slate-700 text-center">{activity.duration}</td>
+            <td className="border border-slate-700 text-center">{activity.difficulty}</td>
+            <td className="border border-slate-700 text-center">
+              <div className="flex justify-center gap-x-4">
+                <Link to={`/activity/details/${activity._id}`}>
+                  <BsInfoCircle className="text-2xl text-green-800" />
+                </Link>
+                <Link to={`/activity/edit/${activity._id}`}>
+                  <AiOutlineEdit className="text-2xl text-yellow-600" />
+                </Link>
+                <Link to={`/activity/delete/${activity._id}`}>
+                  <MdOutlineDelete className="text-2xl text-red-600" />
+                </Link>
+              </div>
+            </td>
+          </tr>
         ))}
       </tbody>
     </table>
