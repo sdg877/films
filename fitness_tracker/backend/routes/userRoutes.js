e224e271f450cc4edfe24344c4d83170439f02b9
Sdg877 <sdrakegill@gmail.com>
2024-12-02 07:16:43 +0000

    Validate user route added

--- fitness_tracker/backend/routes/userRoutes.js
+++ fitness_tracker/backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { create, login, update, checkToken } from "../controllers/users.js";
+import { validateToken } from "../controllers/users.js";
+import authenticateUser from "../middleware/authenticateUser.js";
 import ensureLoggedIn from "../config/ensureLoggedIn.js";
 
 const router = express.Router();
 
 router.get("/check-token", ensureLoggedIn, checkToken);
+router.get("/validate", authenticateUser, validateToken);
 router.post('/signup', create);
 router.post('/login', login);
 router.post("/:id", update);
