860750b95626180cc2c6e0249017b14d3ec23c0a
Sdg877 <sdrakegill@gmail.com>
2024-12-02 10:57:33 +0000

    Removing unused function/route

--- fitness_tracker/backend/index.js
+++ fitness_tracker/backend/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import authenticateUser from "./middleware/authenticateUser.js";
-import activityRoutes from "./routes/activityRoutes.js"
+import activityRoutes from "./routes/activityRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
@@ -22,16 +22,6 @@ app.use(
 
 const DATABASE_URL = process.env.DATABASE_URL;
 const PORT = process.env.PORT || 5555;
-
-app.get("/user/:userId", async (req, res) => {
-  const { userId } = req.params;
-  try {
-    const activities = await Activity.find({ userId });
-    res.json({ data: activities });
-  } catch (error) {
-    res.status(500).json({ error: "Server Error" });
-  }
-});
 
 app.use("/activity", authenticateUser, activityRoutes);
 app.use("/users", userRoutes);
