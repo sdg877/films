6c0fad5bb0800c58044daa32e157414907fe056b
Sdg877 <sdrakegill@gmail.com>
2024-12-03 11:42:27 +0000

    Update error handling for jwt decoding

--- fitness_tracker/backend/middleware/authenticateUser.js
+++ fitness_tracker/backend/middleware/authenticateUser.js
@@ -13,6 +13,7 @@ const authenticateUser = (req, res, next) => {
       return res.status(401).json({ error: "Unauthorized" });
     }
 
+    console.log("Decoded Token:", decoded);
     req.user = decoded.user;
     next();
   });
