6a30793322648123f1a2c3b090348c4bdfc62930
Sdg877 <sdrakegill@gmail.com>
2024-12-05 06:37:30 +0000

    Update to cors policy to allow put requests

--- fitness_tracker/backend/index.js
+++ fitness_tracker/backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:5173",
-    methods: ["GET", "POST", "DELETE"],
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
