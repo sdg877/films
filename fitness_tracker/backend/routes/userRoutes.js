ee7c88cf71b3f9f913bbd26282ac45c28a14e2e0
Sdg877 <sdrakegill@gmail.com>
2024-12-05 08:02:19 +0000

    Get user route implemented

--- fitness_tracker/backend/routes/userRoutes.js
+++ fitness_tracker/backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { create, login, update, checkToken } from "../controllers/users.js";
+import { create, login, update, checkToken, getUser } from "../controllers/users.js";
 import { validateToken } from "../controllers/users.js";
 import authenticateUser from "../middleware/authenticateUser.js";
 import ensureLoggedIn from "../config/ensureLoggedIn.js";
 
 const router = express.Router();
 
+router.get("/:id", getUser);
 router.get("/check-token", ensureLoggedIn, checkToken);
 router.get("/validate", authenticateUser, validateToken);
 router.post('/signup', create);
