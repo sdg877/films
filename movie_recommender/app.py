524bf32f1b14202c8f750318db54210107cdd5d7
Sdg877 <sdrakegill@gmail.com>
2025-01-14 09:27:29 +0000

    Project variables added

--- movie_recommender/app.py
+++ movie_recommender/app.py
@@ -3,3 +3,23 @@ import requests
 import openai
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
+
+load_dotenv()
+TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+openai.api_key = OPENAI_API_KEY
+
+app = Flask(__name__)
+
+GENRE_MAP = {
+    "action": 28,
+    "comedy": 35,
+    "drama": 18,
+    "fantasy": 14,
+    "horror": 27,
+    "romance": 10749,
+    "sci-fi": 878,
+    "thriller": 53
+}
+
